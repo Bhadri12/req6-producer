00f41f443effe8b0eee155930a867e38c20a71dd
Bhadri12 <bhadrirandhi12@gmail.com>
2022-05-18 21:12:37 +0530

    new-commit

---
+++ routes/routes.py
@@ -0,0 +1,18 @@
+from config import app,client
+from flask import request
+from producer import messageSender 
+
+
+@app.route('/upload',methods=['POST'])
+def upload_image():
+    bucket='terraformb24241'
+    content_type=request.mimetype
+    obj=request.files['file']
+    filename=obj.filename
+    client.put_object(Body=obj,
+          Bucket=bucket,
+          Key=filename,
+          ContentType=content_type
+    )
+    messageSender(f'{filename} successfully uploaded')
+    return {'message': 'file uploaded'}
